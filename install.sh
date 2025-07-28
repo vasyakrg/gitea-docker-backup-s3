bc06b456e8f4210f25f444be2e4d495db5f4af2d
Vassiliy Yegorov <va@egorovanet.ru>
2025-07-28 11:35:25 +0700

    Обновление скрипта install.sh для установки awscli через curl и удаление временных файлов

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ set -e
 apk update
 
 # install s3 tools
-apk add python3 py3-awscli
+apk add python3 curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip /tmp/awscliv2.zip -d /tmp
+/tmp/aws/install
+rm -rf /tmp/aws /tmp/awscliv2.zip
 
 # install go-cron
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.7/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
