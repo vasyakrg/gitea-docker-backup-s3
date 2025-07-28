0ead030bf1037528618417ac0fab42a02b6ab342
Vassiliy Yegorov <va@egorovanet.ru>
2025-07-28 11:48:03 +0700

    Добавление установки бинарного файла gitea и создание директории /data в скрипте install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,17 @@ unzip /tmp/awscliv2.zip -d /tmp
 ln -s /usr/local/aws-cli/v2/current/bin/aws /usr/local/bin/aws
 rm -rf /tmp/aws /tmp/awscliv2.zip
 
+# install gitea binary
+curl -L "https://dl.gitea.io/gitea/1.22.3/gitea-1.22.3-linux-amd64" -o /usr/local/bin/gitea
+chmod +x /usr/local/bin/gitea
+
 # install go-cron
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.7/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 
 # make backup directory and change owner to git
-mkdir /backup
-chown git /backup
+mkdir -p /backup /data
+chown git /backup /data
 
 # cleanup
 rm -rf /var/cache/apk/*
